bf5c29fea8fd37e5fd7e394594c9d947e38ccc2b
Chris Womack <womackchrisw@gmail.com>
2024-07-09 19:55:13 -0400

    Add instant withdraw, update commission calculations

--- simulator/src/ui/Simulator.tsx
+++ simulator/src/ui/Simulator.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Accordion } from './Accordion';
 import StringTransformationComponent from '../components/programIdToAddress';
 
+import { creditsProgram } from '../contracts/credits';
+import { arc_0038Program } from '../contracts/arc_0038';
+
 export const Simulator: React.FC = () => {
+  const balanceRef = useRef<HTMLInputElement>(null);
+  const pendingDepositsRef = useRef<HTMLInputElement>(null);
+  const depositRef = useRef<HTMLInputElement>(null);
+  const sharesRef = useRef<HTMLInputElement>(null);
+
+  const calculateNewShares = useCallback(() => {
+  }, [balanceRef, pendingDepositsRef, depositRef, sharesRef]);
+
   return (
     <div>
-      <Accordion title={'Params'}>
+      {/* <Accordion title={'Params'}>
         <div>this is where i would put my params if i had any</div>
       </Accordion>
       <Accordion title={'Simulator'}>
@@ -13,7 +24,21 @@ export const Simulator: React.FC = () => {
       </Accordion>
       <Accordion title={'Program Addresses'}>
         <StringTransformationComponent />
-      </Accordion>
+      </Accordion> */}
+
+
+      <input type="text" id="balance" ref={balanceRef} placeholder="Balance" />
+      <input type="text" id="pending_deposits" ref={pendingDepositsRef} placeholder="Pending Deposits" />
+      <input type="text" id="deposit" ref={depositRef} placeholder="Deposit" />
+      <input type="text" id="total_shares" ref={sharesRef} placeholder="Total Shares" />
     </div>
   )
 };
+
+
+
+const args = process.argv.slice(2);
+const [balance, pending, deposit, shares] = args.map(x => BigInt(x.replace(/[,_]/g, '')));
+const program = new arc_0038Program(new creditsProgram());
+const result = program.inline_calculate_new_shares(balance, pending, deposit, shares);
+console.log(result.toLocaleString());
