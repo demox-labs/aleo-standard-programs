78a8d418aacb87ea9704b4c339031c602e2ea8ab
Julian <julian@demoxlabs.xyz>
2024-08-14 14:16:47 -0700

    Updating lfs settings & adding saved-states

---
+++ pondo-bot/saved-states/programsDeployed/programsDeployed.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b543deba0f9db1a5feab2a8d8f32eef76393953f819a9bdd478e2ef477813a98
+size 150185642
---
+++ pondo-bot/saved-states/programsInitialized/programsInitialized.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:576959f28edd431957a42d73139d795c0a0f6e9e9a5392bcb367c81bf57544c2
+size 96659967
