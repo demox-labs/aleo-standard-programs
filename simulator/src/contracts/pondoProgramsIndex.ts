4ea8516a1c7b3a5abd17983f48659830527a7f59
ZK Clay <hi@zkclay.com>
2024-09-14 20:15:22 -0400

    continue with renames so that the test suite runs.

--- simulator/src/contracts/pondoProgramsIndex.ts
+++ simulator/src/contracts/pondoProgramsIndex.ts
@@ -3,14 +3,14 @@ export { creditsProgram as credits } from "./credits";
 export { token_registryProgram as MTSP } from "./token_registry";
 
 export { pondo_protocolProgram as coreProtocol } from "./pondo_protocol";
-export { validator_oracleProgram as oracle } from "./validator_oracle";
+export { validator_oracleProgram as oracle } from "./pondo_oracle";
 export { reference_delegatorProgram as referenceDelegator } from "./reference_delegator";
 
-export { paleo_tokenProgram as pALEO } from "./paleo_token";
-export { pondo_protocol_tokenProgram as PNDO } from "./pondo_protocol_token";
+export { paleo_tokenProgram as pALEO } from "./pondo_staked_aleo_token";
+export { pondo_protocol_tokenProgram as PNDO } from "./pondo_token";
 
-export { delegator1Program as delegator1 } from "./delegator1";
-export { delegator2Program as delegator2 } from "./delegator2";
-export { delegator3Program as delegator3 } from "./delegator3";
-export { delegator4Program as delegator4 } from "./delegator4";
-export { delegator5Program as delegator5 } from "./delegator5";
+export { delegator1Program as delegator1 } from "./pondo_delegator1";
+export { delegator2Program as delegator2 } from "./pondo_delegator2";
+export { delegator3Program as delegator3 } from "./pondo_delegator3";
+export { delegator4Program as delegator4 } from "./pondo_delegator4";
+export { delegator5Program as delegator5 } from "./pondo_delegator5";
